9a688a1171dbc0e3b226090774770daabf5cd5a9
Arjun143aish <59442901+Arjun143aish@users.noreply.github.com>
2020-05-25 18:42:02 +0530

    Add files via upload

---
+++ salary.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import numpy as np
+
+os.chdir("C:\\Users\\user\\Documents\\Python\\Heroku-Demo-master\\Salary")
+
+X = pd.read_csv("hiring.csv")
+
+X['experience'].fillna(0, inplace =True)
+X['test_score'].fillna(X['test_score'].mean(), inplace =True)
+
+def convert_to_int(word):
+    word_dict = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,
+                 'eight':8,'nine':9,'ten':10,'eleven':11,'twelve':12,'zero':0,0:0}
+    return (word_dict[word])
+
+
+X['experience'] = X['experience'].apply(lambda x: convert_to_int(x))
+
+Df = X.copy()
+
+Y = X.iloc[:,-1]
+
+X.drop(['salary'], axis =1, inplace =True)
+
+from sklearn.linear_model import LinearRegression
+
+M1_Model = LinearRegression().fit(X,Y)
+
+Test_pred =M1_Model.predict(X)
+
+import pickle
+
+pickle.dump(M1_Model,open('model.pkl','wb'))
+model = pickle.load(open('model.pkl','rb'))
